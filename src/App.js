4a1988aa88081d4fd33b54b474b59eb86863681b
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-12-04 20:09:33 +0900

    🎉 프로젝트 생성

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function Header() {
+  return <h1>안녕하시소.</h1>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
