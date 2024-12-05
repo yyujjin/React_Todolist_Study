694c0cc48edee0c0fa60c5554304c493ee9f77ef
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-12-05 12:49:50 +0900

    ✨목록 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ function Body() {
   return;
 }
 
-function MakeTodos({ input, setInput }) {
-  const [todos, setTodos] = useState([]);
-  console.log("찍히고 있음", input);
+function AddTodo(props) {
+  //const [todos, setTodos] = useState([]);
+  //console.log("찍히고 있음", props.input);
   return (
     <button
       onClick={(e) => {
         e.preventDefault();
         alert("추가되었습니다!");
-        setTodos([...todos, input]);
-        console.log("투두스", todos);
-        setInput("");
+        props.setTodos([...props.todos, props.input]);
+        console.log("배열에 저장되고 있는 값", props.todos);
+        props.setInput("");
       }}
     >
       추가하기
@@ -30,9 +30,19 @@ function MakeTodos({ input, setInput }) {
   );
 }
 
-function App() {
-  const [input, setInput] = useState("");
+function MakeTodoList(props) {
+  const todos = props.todos;
+  const list = [];
+  for (let i = 0; i < todos.length; i++) {
+    list.push(<li>{todos[i]}</li>);
+  }
+  return <ul>{list}</ul>;
+}
 
+function App() {
+  //전역 변수로 쓸 건 여기다 쓰면 될듯
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
   console.log(input);
 
   return (
@@ -46,16 +56,15 @@ function App() {
           setInput(e.target.value);
         }}
       />
-      <MakeTodos input={input} setInput={setInput}></MakeTodos>
+      <AddTodo
+        todos={todos}
+        setTodos={setTodos}
+        input={input}
+        setInput={setInput}
+      ></AddTodo>
+      <MakeTodoList todos={todos} setTodos={setTodos}></MakeTodoList>
     </div>
   );
 }
 
 export default App;
-
-// todos 해야할것이라는 배열이 필요함
-// input 칸에 해야할것을 적는다.
-// 버튼을 누르면 input칸이 비어진다.
-// todos 배열에가 todo가 추가되고
-// list로 나타난다.
-//버튼 기본동작 submit 무력화 시키기
