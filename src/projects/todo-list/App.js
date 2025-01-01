a3aa3abb2982075dcd07a79c8449508887562935
yyujjin <141253669+yyujjin@users.noreply.github.com>
2025-01-01 22:35:47 +0900

    ✨ todo 리스트 생성하는 로직 추가

--- src/projects/todo-list/App.js
+++ src/projects/todo-list/App.js
@@ -1,18 +1,14 @@
 import { useState } from "react";
 
 function App() {
-  // <!-- 투두라는 변수를 만들고 초기값은 빈문자열, input에다 값을 입력하고 add 버튼을 누르면 todo 변수의 값이 바뀜  -->
   let [todo, setTodo] = useState("");
   let [todos, setTodos] = useState([]);
 
   const addTodoAndResetInput = () => {
-    //setTodos = todos.push(todo);
-    setTodos([...todos, todo]); // 새로운 배열로 상태 업데이트
+    setTodos([...todos, todo]);
     setTodo("");
     console.log("배열 값 : ", todos);
   };
-
-  const resetInput = () => {};
 
   return (
     <>
@@ -25,16 +21,12 @@ function App() {
         />
       </div>
       <div>
-        {/* <!-- delete버튼을 누르면 todo 변수가 빈 문자열로 초기화 됨 --> */}
         <button>delete</button>
         {/* <!-- update벙튼을 누르면 todo변수 값 옆에 todo1 -> todo1 - 완료 -->
       <!-- todo1 - 완료 이 상태에서 다시 업데이트를 누르면 하이픈 완료 글자가 사라짐  --> */}
         <button>update</button>
       </div>
-      <ul>
-        {/* <!-- todo1 대신 todo 변수넣기 --> */}
-        <li>Todo1</li>
-      </ul>
+      <List todos={todos} />
     </>
   );
 }
@@ -50,6 +42,16 @@ function AddTodo({ todo, setTodo, addTodoAndResetInput }) {
       <button onClick={addTodoAndResetInput}>add</button>
     </>
   );
+}
+
+//배열받아서 for 돌려서 만들기
+function List({ todos }) {
+  const makeList = () => {
+    return todos.map((todo, index) => (
+      <li key={index}>{todo}</li> // JSX 반환
+    ));
+  };
+  return <ul>{makeList()}</ul>;
 }
 
 export default App;
