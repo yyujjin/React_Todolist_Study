d28d452c893818934f96a9f62d64d43aaa806171
yyujjin <141253669+yyujjin@users.noreply.github.com>
2025-01-02 10:30:13 +0900

    🐛 완료 버튼 동작이 반대로 작동하던 버그 수정

--- src/projects/todo-list/App.js
+++ src/projects/todo-list/App.js
@@ -67,7 +67,7 @@ function List({ todos, setTodos }) {
         {/* todo는 객체니까 거기서 key값 빼서 list 만들기  */}
         {/* 이미 중괄호를 썼는데 거기에 밑에처럼 또 중괄호 쓰고 있는데 이건 어케
         작동? */}
-        {todo.status ? todo.todo : <s> {todo.todo}</s>}
+        {todo.status ? <s> {todo.todo}</s> : todo.todo}
         <button onClick={() => updateTodoStatus(i)}>done</button>
       </li>
     ));
